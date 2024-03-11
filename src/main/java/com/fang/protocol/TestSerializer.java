d02decb743364e60077ac457a71cf95c57f512f3
EllenFang <907401621@qq.com>
2024-03-11 16:28:48 +0800

    netty 优化扩展序列化算法

---
+++ src/main/java/com/fang/protocol/TestSerializer.java
@@ -0,0 +1,47 @@
+package com.fang.protocol;
+
+import com.fang.config.Config;
+import com.fang.message.LoginRequestMessage;
+import com.fang.message.Message;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
+import io.netty.channel.embedded.EmbeddedChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+public class TestSerializer {
+
+    public static void main(String[] args) {
+        MessageCodecSharable CODEC = new MessageCodecSharable();
+        LoggingHandler LOGGING = new LoggingHandler(LogLevel.DEBUG);
+        EmbeddedChannel channel = new EmbeddedChannel(LOGGING, CODEC, LOGGING);
+
+        LoginRequestMessage message = new LoginRequestMessage("zhangsan", "123");
+//        channel.writeOutbound(message);
+        ByteBuf byteBuf = messageToByteBuf(message);
+        channel.writeInbound(byteBuf);
+    }
+
+    public static ByteBuf messageToByteBuf(Message msg) {
+        ByteBuf out = ByteBufAllocator.DEFAULT.buffer();
+        // 1. 4 字节的魔数
+        out.writeBytes(new byte[]{1, 2, 3, 4});
+        // 2. 1 字节的版本,
+        out.writeByte(1);
+        // 3. 1 字节的序列化方式 jdk 0 , json 1
+        out.writeByte(Config.getSerializerAlgorithm().ordinal());
+        // 4. 1 字节的指令类型
+        out.writeByte(msg.getMessageType());
+        // 5. 4 个字节
+        out.writeInt(msg.getSequenceId());
+        // 无意义，对齐填充
+        out.writeByte(0xff);
+        // 6. 获取内容的字节数组
+        byte[] bytes = Config.getSerializerAlgorithm().serialize(msg);
+        // 7. 长度
+        out.writeInt(bytes.length);
+        // 8. 写入内容
+        out.writeBytes(bytes);
+        return out;
+    }
+}
